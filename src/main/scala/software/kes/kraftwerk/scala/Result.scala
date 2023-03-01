a6640550134d21c43351540745338a3f5cbe33d4
Kevin Schuetz <schuetzk@gmail.com>
2023-02-28 23:29:08 -0600

    Add value and nextState methods to Result

--- src/main/scala/software/kes/kraftwerk/scala/Result.scala
+++ src/main/scala/software/kes/kraftwerk/scala/Result.scala
@@ -5,6 +5,10 @@ import software.kes.kraftwerk.{Result => JResult}
 case class Result[S, A](toJava: JResult[S, A]) {
   def map[B](f: A => B): Result[S, B] = Result(JResult.result(toJava.getNextState, f(toJava.getValue)))
 
+  def value: A = toJava.getValue
+
+  def nextState: S = toJava.getNextState
+
   def withNextState(newNextState: S): Result[S, A] = Result(toJava.withNextState(newNextState))
 
   def withValue(newValue: A): Result[S, A] = Result(toJava.withValue(newValue))
